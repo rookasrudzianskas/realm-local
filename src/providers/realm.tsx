8f95bdb77da1ad69517edbc00b530e66ba21f902
rokas <rokas@rokas.com>
2024-04-06 11:24:27 +0300

    Add AppProvider to RealmCustomProvider component

--- src/providers/realm.tsx
+++ src/providers/realm.tsx
@@ -1,8 +1,33 @@
 import { PropsWithChildren } from 'react';
 import Realm from 'realm';
-import { RealmProvider } from '@realm/react';
+import { RealmProvider, UserProvider } from '@realm/react';
 import {Task} from "@/src/models/Task";
+import {AppProvider} from "@realm/react/src";
+
+const appId = 'application-0-xqohf';
 
 export default function RealmCustomProvider({ children }: PropsWithChildren) {
-  return <RealmProvider schema={[Task]}>{children}</RealmProvider>;
+  return (
+    <AppProvider id={appId}>
+      {/*<UserProvider fallback={Login}>*/}
+        <RealmProvider
+          schema={[Task]}
+          sync={{
+            flexible: true,
+            onError: (_session, error) => {
+              console.log(error);
+            },
+            initialSubscriptions: {
+              update(subs, realm) {
+                subs.add(realm.objects('Task'));
+              },
+              rerunOnOpen: true,
+            },
+          }}
+        >
+          {children}
+        </RealmProvider>
+      {/*</UserProvider>*/}
+    </AppProvider>
+  )
 }
