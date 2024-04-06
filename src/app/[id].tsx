52657b31cfc710f41a81dd4cf7e457845ebaa16b
rokas <rokas@rokas.com>
2024-04-06 10:42:22 +0300

    Add ts-nocheck and implement updating task description

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,21 +1,40 @@
+// @ts-nocheck
 import { Stack, useLocalSearchParams } from 'expo-router';
-import { View, Text } from 'react-native';
-import { useObject } from '@realm/react';
+import {View, Text, TextInput} from 'react-native';
+import {useObject, useRealm} from '@realm/react';
 import { Task } from '../models/Task';
 import { BSON } from 'realm';
+import {useState} from "react";
 
 const TaskDetails = () => {
   const { id } = useLocalSearchParams();
+  const realm = useRealm();
+
   const task = useObject<Task>(Task, new BSON.ObjectID(id as string));
+  const [updatedDescription, setUpdatedDescription] = useState(
+    task?.description
+  );
 
   if(!task) return null;
+
+  const updateDescription = () => {
+    if(!task) return;
+    realm.write(() => {
+      task.description = updatedDescription;
+    });
+  }
 
   return (
     <View style={{ padding: 10 }}>
       <Stack.Screen options={{ title: 'Task Details' }} />
 
       <Text style={{ color: 'white', fontSize: 20 }}>Id: {id}</Text>
-      <Text style={{ color: 'white', fontSize: 20 }}>Task: {task.description}</Text>
+      <TextInput
+        className={'bg-[#27272a] px-3 py-2 rounded-md my-3 text-white'}
+        onChangeText={setUpdatedDescription}
+        onEndEditing={updateDescription}
+        value={updatedDescription}
+      />
     </View>
   );
 };
