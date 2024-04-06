99868690c9652f185208c60205c7760ecc4b1f80
rokas <rokas@rokas.com>
2024-04-06 09:31:04 +0300

    Refactor tab layout and task list item components

--- src/components/task-list-item.tsx
+++ src/components/task-list-item.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import {Text, View, StyleSheet, TouchableOpacity} from 'react-native';
 import {Ionicons} from "@expo/vector-icons";
+import {router} from "expo-router";
 
 interface task {
   task: {
@@ -16,14 +17,18 @@ const TaskListItem = ({task}: task) => {
   }
 
   return (
-    <View className={'bg-[#27272a] rounded-md px-3 py-2 flex-row items-center justify-between flex'}>
+    <TouchableOpacity onPress={() => router.push(`/${task.id}`)} className={'bg-[#27272a] rounded-md px-3 py-2' +
+      ' flex-row' +
+      ' items-center' +
+      ' justify-between' +
+      ' flex'}>
       <Text className={'text-white'}>
         {task.description}
       </Text>
       <TouchableOpacity activeOpacity={0.7} onPress={deleteTask}>
         <Ionicons name="close-outline" size={17} color="white" />
       </TouchableOpacity>
-    </View>
+    </TouchableOpacity>
   );
 };
 
